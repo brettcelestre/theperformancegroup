694db11448aed23e5fbdaf75c81e4bd05cbc1d5e
Brett Celestre <bcelestre@gmail.com>
2016-12-01 17:43:05 -0800

    Sets up footer component and styles

--- client/components/footer.js
+++ client/components/footer.js
@@ -19,7 +19,31 @@ class Footer extends Component {
   render() {
     return (
       <footer className='footer'>
-        <h2>Footer</h2>
+        <div className='container'>
+          <div className='row'>
+            <div className='three columns testt'>
+              <img src='../assets/imgs/footer-logo.png' width='206px' height='50px' className='footer-logo' />
+              <p>
+                6673 Owens Drive
+                Pleasanton, CA 94588
+                Email: contact@tpgsales.com
+                Phone: (925) 555-5555
+              </p>
+            </div>
+            <div className='three columns'>
+              <h4>holder text</h4>
+            </div>
+            <div className='three columns'>
+              <h4>holder text</h4>
+            </div>
+            <div className='three columns'>
+              <h4>holder text</h4>
+            </div>
+          </div>
+        </div>
+        <div className='container'>
+          <h5>2016 THE PERFORMANCE GROUP. ALL RIGHTS RESERVED.</h5>
+        </div>
       </footer>
     );
   }
