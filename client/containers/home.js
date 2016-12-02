079b93065df9fa8879ead9c4b5a3b515b59887a9
Brett Celestre <bcelestre@gmail.com>
2016-12-01 19:19:26 -0800

    Updates home container's copy

--- client/containers/home.js
+++ client/containers/home.js
@@ -34,7 +34,90 @@ class Home extends Component {
  	// Render
 	render() {
 		return (
-			<h2>Home</h2>
+			<div className='home'>
+				
+				<div className='drop-shadow'>
+				</div>
+				
+				<div className='banner'>
+				</div>
+				
+				<div className='container'>
+					<div className='row'>
+						<h4>Why We Started</h4>
+					</div>
+					<div className='row'>
+						<p>The evolution of the traditional food broker has led to the formation of large Regional and National organizations. Regardless of size, these companies were all faced with the same problems:</p>
+					</div>
+					<div className='row'>
+						<div className='one-third column'>
+							<h6>logo</h6>
+						</div>
+						<div className='one-third column'>
+							<h6>logo</h6>
+						</div>
+						<div className='one-third column'>
+							<h6>logo</h6>
+						</div>
+					</div>
+					<div className='row'>
+						<div className='one-third column'>
+							<h6>logo</h6>
+						</div>
+						<div className='one-third column'>
+							<h6>logo</h6>
+						</div>
+						<div className='one-third column'>
+							<h6>logo</h6>
+						</div>
+					</div>
+					<div className='row'>
+						<div className='one-third column'>
+							<h6>logo</h6>
+						</div>
+						<div className='one-third column'>
+							<h6>logo</h6>
+						</div>
+						<div className='one-third column'>
+							<h6>logo</h6>
+						</div>
+					</div>
+				</div>
+				
+				<div className='key-dynamics'>
+					<div className='container'>
+						<div className='row'>
+							<h4>Key Dynamics</h4>
+						</div>
+						<div className='row'>
+							<p> copy </p>
+						</div>
+						<div className='row'>
+							<p> bullet point copy </p>
+						</div>
+					</div>
+				</div>
+				
+				<div className='lets-talk'>
+					<div className='container'>
+						<div className='row'>
+							<div className='eight columns'>
+								<div className='row'>
+									<h2>Lets Talk</h2>
+								</div>
+								<div className='row'>
+									<p> copy </p>
+								</div>
+							</div>
+							<div className='four columns'>
+								<button>CONTACT</button>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				
+			</div>
 		);	
 	}
 }
