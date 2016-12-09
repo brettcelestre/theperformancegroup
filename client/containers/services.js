b9183af5d10db3e3efcedb8f5fe8bb88832e8727
Brett Celestre <bcelestre@gmail.com>
2016-12-09 15:42:31 -0800

    Adds content to services container

--- client/containers/services.js
+++ client/containers/services.js
@@ -20,7 +20,46 @@ class Services extends Component {
         <div className='services-banner'>
         </div>
       
-        <h2>Services</h2>
+        <div className='container section-spacer'>
+          <div className='row'>
+            <h3 className='section-header'>Services</h3>
+            <div className='copy'>
+              With over 75 combined years of experience in the Northern California Market, we offer:
+            </div>
+          </div>
+          
+          <div className='row copy'>
+            - Local Ownership with unequaled reputation/respect in the market.<br />
+            - We are committed to independent, entrepreneurial & participative ownership.<br />
+            - We are not burdened by traditional broker thinking and actions.<br />
+            - We are a results driven sales services provider.<br />
+            - Account management, coordination and communication, locally or nationally.<br />
+            - Ongoing enhancement of administrative support services.<br />
+            - Ongoing infrastructure investment, refinement and development.<br />
+            - We feel strongly about reinvesting in our business.<br />
+            - Category experience and expertise in:<br />
+                  <div className='tab'>Grocery<br />
+                  Refrigerated<br />
+                  Frozen Food<br />
+                  Confection & Snack Foods<br />
+                  Specialty Foods<br />
+                  Non-Foods<br />
+                  Ethnic</div>
+            - Safeway Corporate coverage:<br />
+                  <div className='tab'>Headquarter Coverage<br />
+                  Order entry/Deduction management<br />
+                  Tracking<br />
+                  Forms Management<br />
+                  Fund Management<br />
+                  Excellent Communication</div>
+            - We strive to be problem solvers and solution providers.<br />
+            - Our services are tailored to compliment your strengths and help you to reach your goals.<br />
+            - Coverage for all classes of trade.<br />
+            - Complete retail menu of services.
+          </div>
+        
+        </div>
+        
       </div>
     );  
   }
